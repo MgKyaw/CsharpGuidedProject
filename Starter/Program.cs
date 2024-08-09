fd45bb92a777b3a8fecd6194c19caf0919605262
MgKyaw <mgkyaw20112@gmail.com>
2024-08-09 12:43:27 +0700

    feat: Modify getting sum assignment scores for the extra credit points

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,10 +1,14 @@
-﻿int currentAssignments = 5;
-int[] sophiaScores = [90, 86, 87, 98, 100];
-int[] andrewScores = [92, 89, 81, 96, 90];
-int[] emmaScores = [90, 85, 87, 98, 68];
-int[] loganScores = [90, 95, 87, 88, 96];
+﻿int examAssignments = 5;
+
+int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
+int[] andrewScores = [92, 89, 81, 96, 90, 89];
+int[] emmaScores = [90, 85, 87, 98, 68, 89, 89, 89];
+int[] loganScores = [90, 95, 87, 88, 96, 96];
+
 string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
+
 int[] studentScores = new int[10];
+
 string currentStudentLetterGrade = "";
 
 Console.WriteLine("Student\t\tScores\tGrade\n");
@@ -14,32 +18,36 @@ foreach (string name in studentNames)
     string currentStudent = name;
 
     if (currentStudent == "Sophia")
-    {
-        studentScores = sophiaScores;
-    }
+       studentScores = sophiaScores;
+
     else if (currentStudent == "Andrew")
-    {
         studentScores = andrewScores;
-    }
+
     else if (currentStudent == "Emma")
-    {
         studentScores = emmaScores;
-    }
+
     else if (currentStudent == "Logan")
-    {
         studentScores = loganScores;
-    }
 
     int sumAssignmentScores = 0;
 
     decimal currentStudentGrade = 0;
 
+    int gradedAssignments = 0;
+
     foreach (int score in studentScores)
     {
-        sumAssignmentScores += score;
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            sumAssignmentScores += score;
+
+        else
+            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+            sumAssignmentScores += score / 10;
     }
 
-    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
@@ -76,11 +84,12 @@ foreach (string name in studentNames)
 
     else if (currentStudentGrade >= 60)
         currentStudentLetterGrade = "D-";
+
     else
         currentStudentLetterGrade = "F";
 
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
-Console.WriteLine("Press the Enter key to continue");
+Console.WriteLine("\n\rPress the Enter key to continue");
 Console.ReadLine();
