544ea107423b1430acd0fa555a3f4e04f6ed4f94
MgKyaw <mgkyaw20112@gmail.com>
2024-08-09 12:33:36 +0700

    feat: Add if else block to get StudentLetterGrade

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -5,8 +5,9 @@ int[] emmaScores = [90, 85, 87, 98, 68];
 int[] loganScores = [90, 95, 87, 88, 96];
 string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
 int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
 
-Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Student\t\tScores\tGrade\n");
 
 foreach (string name in studentNames)
 {
@@ -40,7 +41,45 @@ foreach (string name in studentNames)
 
     currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
 
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
 Console.WriteLine("Press the Enter key to continue");
